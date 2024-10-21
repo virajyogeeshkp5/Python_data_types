576feaa01592c834684034ec2508506125e893cc
YOGEESH K P <yogeesh793@gmail.com>
2024-10-21 12:02:13 +0530

    Add files via upload

---
+++ Python_data_types.py
@@ -0,0 +1,45 @@
+# Python Data Types
+# Numeric
+# integer
+a=25
+print(type(a))
+
+# float
+a=8.50
+print(type(a))
+
+# string
+a='Keyboard'
+print(type(a))
+
+# Complex
+z=5+6j
+print(type(z))
+
+print(z.real)
+
+print(z.imag)
+
+# boolean
+m=True
+print(type(m))
+
+# list
+list=[1,2,3]
+print(type(list))
+
+# Tuple
+t=(50,20,30,10,40,50)
+print(type(t))
+
+# Set
+s={1,2,3}
+print(type(s))
+
+# dictinory
+d={'age':[15,16,18],'height':[6.6,6.2,5.8]}
+print(type(d))
+
+# none
+c=None
+print(type(c))
